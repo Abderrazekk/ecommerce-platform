6e6c8e001aeac0a811393f56e2c6e3ef77c0b397
Abderazek <abderazekbelhadjmbarek@gmail.com>
2026-03-01 23:38:40 +0100

    clean login page

--- ecommerce-backend/src/utils/email.js
+++ ecommerce-backend/src/utils/email.js
@@ -1,20 +1,24 @@
 // utils/email.js
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   // Create transporter (use environment variables)
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false, // avoids SSL issues with OVH
     },
   });
 
   // Define email options
   const mailOptions = {
-    from: `"E‑Commerce" <${process.env.EMAIL_FROM}>`,
+    from: `"Shopina" <${process.env.EMAIL_FROM}>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
